415c89fd7f19018ce14dbdaa95904303181c52b1
Ernest <ernest.bruno94@yahoo.com>
2018-04-03 15:38:12 -0700

    Edit battle bug where character gains health when enemy damage is less than 0

--- src/app/battle-seq/battle-seq.component.ts
+++ src/app/battle-seq/battle-seq.component.ts
@@ -24,12 +24,16 @@ export class BattleSeqComponent {
 
   charAttack(){
     this.childCurrentCharacter.attack(this.currentMonster);
-    this.logs.push(`${this.childCurrentCharacter.name} attacked ${this.currentMonster.name} for ${Math.ceil(this.childCurrentCharacter.str * 1.5)}`);
+    this.logs.push(`${this.childCurrentCharacter.name} attacked ${this.currentMonster.name} for ${Math.ceil(this.childCurrentCharacter.str * 1.5)} damage`);
   }
 
   monsterAttack(){
     this.currentMonster.attack(this.childCurrentCharacter);
-    this.logs.push(`${this.currentMonster.name} attacked ${this.childCurrentCharacter.name} for ${Math.ceil((this.currentMonster.attackDamage * (1.0 + (this.currentMonster.level / 10.0))) - (this.childCurrentCharacter.def * (0.8)))}`);
+    let damage: number = Math.ceil((this.currentMonster.attackDamage * (1.0 + (this.currentMonster.level / 10.0))) - (this.childCurrentCharacter.def * (0.8)));
+    if (damage < 0){
+      damage = 0;
+    }
+    this.logs.push(`${this.currentMonster.name} attacked ${this.childCurrentCharacter.name} for ${damage} damage`);
   }
 
   showItems(){
