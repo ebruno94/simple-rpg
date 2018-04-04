92aff7fad7d109fa5ea283ac4a361052b66903f7
Ernest <ernest.bruno94@yahoo.com>
2018-04-04 12:17:23 -0700

    Add setCharacter function

--- src/app/character-select/character-select.component.ts
+++ src/app/character-select/character-select.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Player } from './../models/Player';
+import { Character } from './../models/Character';
 
 @Component({
   selector: 'app-character-select',
@@ -13,6 +14,10 @@ export class CharacterSelectComponent implements OnInit {
   ngOnInit() {
   }
 
+  selectedCharacter: Character;
   currentPlayer: Player = new Player("Ernest", "ernnex", "ernnex");
 
+  setCharacter(char){
+    this.selectedCharacter = char;
+  }
 }
